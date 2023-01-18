7585970ebaf5a396f153985157bc39e27c40eeed
toMilanesi <tomi.milanesi@gmail.com>
2023-01-18 16:07:56 -0300

    Add Header component with placeholder and nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/header/Header';
+
 function App() {
   return (
     <div className="bg-gray-600 h-screen">
-      <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 }
