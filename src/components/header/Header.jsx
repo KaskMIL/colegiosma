366870985664b10af8ebd323dcfccb8c2ad66d4f
toMilanesi <tomi.milanesi@gmail.com>
2023-02-01 22:31:28 -0300

    Fix responsive header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../navbar/Navbar';
 
 const Header = () => (
-  <header className="text-purple-500 text-4xl flex items-end justify-between px-[100px]">
+  <header className="bg-purple-500">
     <NavBar />
   </header>
 );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,34 +8,32 @@ export default function NavBar() {
 
   return (
     <nav className='w-full bg-white shadow'>
-      <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
-        <div>
-          <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-            <h2 className='text-2xl font-bold'>LOGO</h2>
-            <div className='md:hidden'>
-              <button
-                className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
-                type='button'
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? <GrClose /> : <FaBars />}
-              </button>
-            </div>
+      <div className='px-4 mx-auto lg:max-w-7xl md:px-8'>
+        <div className='flex items-center justify-between py-2 w-full md:py-5 md:block'>
+          <h2 className='text-2xl font-bold md:bg-purple-300 md:flex md:justify-center md:py-10'>LOGO</h2>
+          <div className='md:hidden'>
+            <button
+              className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
+              type='button'
+              onClick={() => setNavbar(!navbar)}
+            >
+              {navbar ? <GrClose /> : <FaBars />}
+            </button>
           </div>
         </div>
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? 'block' : 'hidden'
-            }`}
-          >
-            <ul className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
-              <li className='text-gray-600 hover:text-blue-600'>Home</li>
-              <li className='text-gray-600 hover:text-blue-600'>Blog</li>
-              <li className='text-gray-600 hover:text-blue-600'>About US</li>
-              <li className='text-gray-600 hover:text-blue-600'>Contact US</li>
-            </ul>
-          </div>
+      </div>
+      <div>
+        <div
+          className={`flex-1 justify-self-center py-[1px] md:block ${
+            navbar ? 'block' : 'hidden'
+          }`}
+        >
+          <ul className='flex flex-col items-center justify-center space-y-8 md:flex-row md:space-x-6 md:space-y-0'>
+            <li className='nav-hov text-gray-600 hover:text-blue-600'>Inicio</li>
+            <li className='nav-hov text-gray-600 hover:text-blue-600'>Niveles</li>
+            <li className='nav-hov text-gray-600 hover:text-blue-600'>Propuesta</li>
+            <li className='nav-hov text-gray-600 hover:text-blue-600'>Contacto</li>
+          </ul>
         </div>
       </div>
     </nav>
