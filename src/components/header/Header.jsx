9884c82b5d677d5ae30274c7812f93a442089720
toMilanesi <tomi.milanesi@gmail.com>
2023-05-22 08:51:18 -0300

    Fix header and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { BsFillTelephoneFill } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
 import NavBar from '../navbar/Navbar';
-import logoSma from '../../assets/img/logo-escudo-sma.png';
-import logoLav from '../../assets/img/logo-escudo-lav.png';
+import logoSma from '../../assets/img/logo-csma_1.png';
+import logoLav from '../../assets/img/logo-jardin.png';
 
 const Header = () => (
   <header className='bg-white'>
     <div className='flex justify-between px-3 bg-gray-700 text-white py-2 shadow-xl gap-2'>
-      <button className='bg-[#009245] py-1 px-5 rounded text-[10px] sm:text-base' type='button'>Solicitar Entrevista</button>
+      <button className='bg-[#009245] py-1 px-5 rounded text-[10px] sm:text-base hover:bg-white hover:text-[#009245]' type='button'>Solicitar Entrevista</button>
       <div className='gap-7 text-gray-300 text-xs sm:flex sm:text-base'>
         <div className='flex items-center gap-1'>
           <BsFillTelephoneFill />
@@ -21,9 +21,9 @@ const Header = () => (
         </div>
       </div>
     </div>
-    <div className='flex items-center justify-around pt-5'>
-      <img className='h-auto' src={logoSma} alt='Logo colegio' />
-      <img src={logoLav} alt='Logo jardin' />
+    <div className='flex items-center justify-around sm:justify-center sm:gap-[200px] py-3'>
+      <img className='w-[100px] sm:w-[200px] h-auto' src={logoSma} alt='Logo colegio' />
+      <img className='w-[100px] sm:w-[200px] h-auto' src={logoLav} alt='Logo jardin' />
     </div>
     <NavBar />
   </header>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,16 +9,14 @@ export default function NavBar() {
   return (
     <nav className='w-full bg-white shadow'>
       <div className='px-4 mx-auto lg:max-w-7xl md:px-8'>
-        <div className='flex items-center justify-between py-2 w-full md:py-5 md:block'>
-          <div className='md:hidden'>
-            <button
-              className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
-              type='button'
-              onClick={() => setNavbar(!navbar)}
-            >
-              {navbar ? <GrClose /> : <FaBars />}
-            </button>
-          </div>
+        <div className='md:hidden flex justify-end'>
+          <button
+            className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
+            type='button'
+            onClick={() => setNavbar(!navbar)}
+          >
+            {navbar ? <GrClose /> : <FaBars />}
+          </button>
         </div>
       </div>
       <div>
@@ -27,12 +25,16 @@ export default function NavBar() {
             navbar ? 'block' : 'hidden'
           }`}
         >
-          <ul
-            className={`navbar ${navbar ? null : 'mx-[100px]'}`}
-          >
-            <li className='nav-hov text-white hover:scale-120 bree-font'>Inicio</li>
-            <li className='nav-hov text-white hover:scale-120 bree-font'>Historia</li>
-            <li className='nav-hov text-white hover:scale-120 bree-font'>Contacto</li>
+          <ul className={`navbar ${navbar ? null : 'mx-[100px]'}`}>
+            <li className='nav-hov text-white hover:scale-120 bree-font'>
+              Inicio
+            </li>
+            <li className='nav-hov text-white hover:scale-120 bree-font'>
+              Historia
+            </li>
+            <li className='nav-hov text-white hover:scale-120 bree-font'>
+              Contacto
+            </li>
             {/*
             <li className='nav-hov text-gray-600 hover:text-blue-600 bree-font'>Contacto</li>
             */}
