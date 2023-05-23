c55ba63766d9bc0bae4f3855534649f9c25e2ad9
toMilanesi <tomi.milanesi@gmail.com>
2023-05-23 11:28:03 -0300

    Add styles and fix responsiveness

--- src/components/contact/Contacto.jsx
+++ src/components/contact/Contacto.jsx
@@ -4,16 +4,16 @@ import { FaMapMarkerAlt } from 'react-icons/fa';
 import MapComp from '../map/Map';
 
 const Contacto = () => (
-  <section className="flex flex-col items-center py-5 bg-purple-400">
-    <h2>Solicitá tu entrevista!</h2>
-    <ul className="flex justify-around w-full pb-5">
+  <section className="flex flex-col items-center pt-5 md:py-5 bg-white">
+    <h2 className="bree-font text-xl md:text-5xl tracking-[0.1rem] font-bold mb-5">Solicitá tu entrevista!</h2>
+    <ul className="flex justify-around w-full bg-slate-600 text-white py-5 mb-5 md:text-xl">
       <li className="flex gap-2 items-center">
         <BsFillTelephoneFill />
-        4455-5400
+        <a href="tel:44555400">4455-5400</a>
       </li>
       <li className="flex gap-2 items-center">
         <FaMapMarkerAlt />
-        San Jose 5290
+        San Jose 5290, San Miguel
       </li>
     </ul>
     <MapComp />
